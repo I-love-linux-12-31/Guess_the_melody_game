09b40912130b6fb92f1a9102fe115de78073cc3b
Yaroslav Kuznetsov <I-love-linux-12-31@users.noreply.github.com>
2024-11-17 17:04:25 +0300

    Updated build script for compatibility with python3.13+

--- build.sh
+++ build.sh
@@ -2,4 +2,11 @@
 python3 -m venv venv || echo "venv already exists";
 source venv/bin/activate
 pip3 install -r requirements.txt
+
+# Check Python version and install audioop-lts if 3.13 or higher
+python_version=$(python3 --version | cut -d ' ' -f 2)
+if [[ $(echo "$python_version 3.13" | awk '{print ($1 >= $2)}') -eq 1 ]]; then
+    pip3 install audioop-lts
+fi
+
 mkdir resources/music || echo "resources/music already exists";
